78c3bb686959595061c20b41b6e9ea996972a205
LorenzoVeropalumbo <lorenzo-veropalumbo@outlook.it>
2022-06-27 14:44:28 +0200

    implementing custom feature

--- customFeature/js/script.js
+++ customFeature/js/script.js
@@ -24,20 +24,24 @@ var app = new Vue(
         methods: {
             addNewTodo() {
                 if(this.newTodoText.length > 0) {
-                    
                     // Pushare nell'array newTodoText
                     this.newTodoObj = {
                         'text': this.newTodoText,
                         'done': false,
-                    };
-                    this.copyTodoObj = {...this.newTodoObj};
+                    }
+                    this.copyTodoObj = {...this.newTodoObj}
                     this.todos.push(this.copyTodoObj);
                     this.newTodoText = '';
+                    this.copyTodoObj = {};
+                    this.newTodoObj = {};
                 }
             },
             removeTodo(index) {
                 this.todos.splice(index, 1);
             },
+            TodoOrNotTodo(singleTodo) {
+                singleTodo.done = !singleTodo.done;
+            }
         }
     }
 );
