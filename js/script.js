82d2b79b9dc7e757b3d8dcace810fb0d69c6b6e4
LorenzoVeropalumbo <lorenzo-veropalumbo@outlook.it>
2022-06-27 13:43:56 +0200

    end

--- js/script.js
+++ js/script.js
@@ -12,11 +12,11 @@ var app = new Vue(
                     done: false,
                 },
                 {
-                    text: 'Fare i compiti',
+                    text: 'Fare la spesa',
                     done: true,
                 },
                 {
-                    text: 'Fare i compiti',
+                    text: 'Fare il bucato',
                     done: false,
                 }
             ]
